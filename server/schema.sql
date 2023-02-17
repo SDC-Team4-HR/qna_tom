4c00505cadaa57789c8c7793427704e55f59eb91
Tom Spitz <tjspitz1986@gmail.com>
2023-02-16 18:47:21 -0600

    Implementation that has copied csv data to postgres db. Is lacking the "E" in "ETL" though.

--- server/schema.sql
+++ server/schema.sql
@@ -1,60 +1,64 @@
 -- ---
 -- Database 'sdc_qna'
 -- ---
--- DROP DATABASE IF EXISTS sdc_qna;
--- CREATE DATABASE sdc_qna;
 
 -- ---
 -- Table 'questions'
 --
 -- ---
 
-DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS questions cascade;
 
 CREATE TABLE questions (
   id SERIAL,
-  question_body TEXT NOT NULL,
-  question_date TIMESTAMP NOT NULL,
-  asker_name VARCHAR(255) NOT NULL,
-  asker_email VARCHAR(255) NOT NULL,
-  question_helpfulness INTEGER DEFAULT 0,
-  reported BOOLEAN DEFAULT FALSE,
   product_id INTEGER NOT NULL,
+  question_body TEXT,
+  question_date BIGINT,
+  asker_name VARCHAR(255),
+  asker_email VARCHAR(255),
+  reported BOOLEAN,
+  question_helpfulness INTEGER DEFAULT 0,
   PRIMARY KEY (id)
 );
+
+CREATE INDEX product_id_idx on questions(product_id);
 
 -- ---
 -- Table 'answers'
 --
 -- ---
 
-DROP TABLE IF EXISTS answers;
+DROP TABLE IF EXISTS answers cascade;
 
 CREATE TABLE answers (
   id SERIAL,
-  answer_body TEXT NOT NULL,
-  answer_date TIMESTAMP NOT NULL,
-  answerer_name VARCHAR(255) NOT NULL,
-  answerer_email VARCHAR(255) NOT NULL,
-  answer_helpfulness INTEGER DEFAULT 0,
-  reported BOOLEAN DEFAULT FALSE,
   question_id INTEGER NOT NULL,
+  answer_body TEXT,
+  answer_date BIGINT,
+  answerer_name VARCHAR(255),
+  answerer_email VARCHAR(255),
+  reported BOOLEAN,
+  answer_helpfulness INTEGER DEFAULT 0,
   PRIMARY KEY (id)
 );
+
+CREATE INDEX question_id_idx on answers(question_id);
 
 -- ---
 -- Table 'photos'
 --
 -- ---
 
-DROP TABLE IF EXISTS photos;
+DROP TABLE IF EXISTS photos cascade;
 
 CREATE TABLE photos (
   id SERIAL,
-  url VARCHAR(255) NOT NULL,
   answer_id INTEGER NOT NULL,
+  url VARCHAR(255),
   PRIMARY KEY (id)
 );
+
+CREATE INDEX answer_id_idx on photos(answer_id);
 
 -- ---
 -- Foreign Keys
@@ -64,12 +68,23 @@ ALTER TABLE answers ADD FOREIGN KEY (question_id) REFERENCES questions (id);
 ALTER TABLE photos ADD FOREIGN KEY (answer_id) REFERENCES answers (id);
 
 -- ---
--- Table Properties
+-- Populate tables
 -- ---
 
--- ALTER TABLE questions ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE answers ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE photos ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+COPY questions
+FROM '/Users/tjspitz/Coding/HackReactor/RFP_2212/rfp2212-sdc-doritos/server/dataImports/questions.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY answers
+FROM '/Users/tjspitz/Coding/HackReactor/RFP_2212/rfp2212-sdc-doritos/server/dataImports/answers.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY photos
+FROM '/Users/tjspitz/Coding/HackReactor/RFP_2212/rfp2212-sdc-doritos/server/dataImports/answers_photos.csv'
+DELIMITER ','
+CSV HEADER;
 
 -- ---
 -- BATCH FILE
